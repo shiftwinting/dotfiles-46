51e5fb3c65a98639a16f2aac01e0a90ff7637e6d
Shivansh-007 <shivansh-007@outlook.com>
2021-07-16 14:56:04 +0530

    Lazy load all plugins

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,59 +1,20 @@
 -- load all plugins
 require "pluginList"
-require "misc-utils"
+require "options"
+require "plugins.bufferline"
 
-require "top-bufferline"
-require "statusline"
-
-require("neoscroll").setup() -- smooth scroll
-
--- lsp stuff
-require "nvim-lspconfig"
-require "compe-completion"
-
-local cmd = vim.cmd
 local g = vim.g
 
 g.mapleader = " "
-g.auto_save = 0
+g.auto_save = false
 
 -- colorscheme related stuff
-cmd "syntax on"
 
--- Tokynight Configuration
-g.tokyonight_style = "storm" -- "light"
-g.tokyonight_italic_functions = true
-g.tokyonight_transparent = true
-cmd[[colorscheme tokyonight]]
+g.nvchad_theme = "onedark"
+local base16 = require "base16"
+base16(base16.themes["onedark"], true)
 
--- blankline
-
-g.indentLine_enabled = 1
-g.indent_blankline_char = "▏"
-
-g.indent_blankline_filetype_exclude = {"help", "terminal", "dashboard"}
-g.indent_blankline_buftype_exclude = {"terminal"}
-
-g.indent_blankline_show_trailing_blankline_indent = false
-g.indent_blankline_show_first_indent_level = false
-
-require "treesitter-nvim"
+require "highlights"
 require "mappings"
 
-require "telescope-nvim"
-require "nvimTree" -- file tree stuff
-require "file-icons"
-
--- git signs , lsp symbols etc
-require "gitsigns-nvim"
-require("nvim-autopairs").setup()
-require("lspkind").init()
-
--- hide line numbers in terminal windows
-vim.api.nvim_exec([[
-   au BufEnter term://* setlocal nonumber
-]], false)
-
-require "zenmode"
-require "whichkey"
-
+require("utils").hideStuff()
--- .config/nvim/lua/pluginList.lua
+++ .config/nvim/lua/pluginList.lua
@@ -1,70 +1,219 @@
-return require("packer").startup(
+local packer = require("packer")
+local use = packer.use
+
+packer.init {
+    display = {
+        open_fn = function()
+            return require("packer.util").float {border = "single"}
+        end
+    },
+    git = {
+        clone_timeout = 600 -- Timeout, in seconds, for git clones
+    }
+}
+
+return packer.startup(
     function()
         use "wbthomason/packer.nvim"
 
-        -- color/UI related stuff
-        use "folke/tokyonight.nvim"
-        use "norcalli/nvim-colorizer.lua"
-
-        -- lang/lsp stuff
-        use "nvim-treesitter/nvim-treesitter"
-        use "neovim/nvim-lspconfig"
-        use "hrsh7th/nvim-compe"
-        use "onsails/lspkind-nvim"
-        use "sbdchd/neoformat"
-        use "nvim-lua/plenary.nvim"
-        use "kabouzeid/nvim-lspinstall"
-        use "folke/trouble.nvim"
-        use "folke/lsp-colors.nvim"
-        use "kosayoda/nvim-lightbulb"
-        use "ray-x/lsp_signature.nvim"
-
-        use "lewis6991/gitsigns.nvim"
         use "akinsho/nvim-bufferline.lua"
-        use "glepnir/galaxyline.nvim"
-        use "windwp/nvim-autopairs"
-        use "alvan/vim-closetag"
 
-        -- snippet support
-        -- use "hrsh7th/vim-vsnip"
-        -- use "rafamadriz/friendly-snippets"
+        use {
+            "glepnir/galaxyline.nvim",
+            config = function()
+                require("plugins.statusline").config()
+            end
+        }
+
+        -- color related stuff
+        use "siduck76/nvim-base16.lua"
+
+        use {
+            "norcalli/nvim-colorizer.lua",
+            event = "BufRead",
+            config = function()
+                require("colorizer").setup()
+                vim.cmd("ColorizerReloadAllBuffers")
+            end
+        }
+
+        -- language related plugins
+        use {
+            "nvim-treesitter/nvim-treesitter",
+            event = "BufRead",
+            config = function()
+                require("plugins.treesitter").config()
+            end
+        }
+
+        use {
+            "kabouzeid/nvim-lspinstall",
+            event = "BufRead"
+        }
+
+        use {
+            "neovim/nvim-lspconfig",
+            after = "nvim-lspinstall",
+            config = function()
+                require("plugins.lspconfig").config()
+            end
+        }
+
+        use {
+            "onsails/lspkind-nvim",
+            event = "BufRead",
+            config = function()
+                require("lspkind").init()
+            end
+        }
+
+        -- load compe in insert mode only
+        use {
+            "hrsh7th/nvim-compe",
+            event = "InsertEnter",
+            config = function()
+                require("plugins.compe").config()
+            end,
+            wants = {"LuaSnip"},
+            requires = {
+                {
+                    "L3MON4D3/LuaSnip",
+                    wants = "friendly-snippets",
+                    event = "InsertCharPre",
+                    config = function()
+                        require("plugins.compe").snippets()
+                    end
+                },
+                {
+                    "rafamadriz/friendly-snippets",
+                    event = "InsertCharPre"
+                }
+            }
+        }
+
+        use {"sbdchd/neoformat", cmd = "Neoformat"}
 
         -- file managing , picker etc
-        use "kyazdani42/nvim-tree.lua"
-        use "kyazdani42/nvim-web-devicons"
-        use "ryanoasis/vim-devicons"
-        use "nvim-telescope/telescope.nvim"
-        use "nvim-telescope/telescope-media-files.nvim"
-        use "nvim-lua/popup.nvim"
-
-        -- misc
-        use {'iamcco/markdown-preview.nvim', config = "vim.call('mkdp#util#install')"}
-        use "sudormrfbin/cheatsheet.nvim"
-        use "907th/vim-auto-save"
-        use "karb94/neoscroll.nvim"
-        use "folke/zen-mode.nvim"
-        use "folke/which-key.nvim"
-        use {"lukas-reineke/indent-blankline.nvim", branch = "lua"}
-
-        -- Configure and list todo-comments	
         use {
-          "folke/todo-comments.nvim", 
-          config = function()	
-            require("todo-comments").setup()	
-          end
+            "kyazdani42/nvim-tree.lua",
+            cmd = "NvimTreeToggle",
+            config = function()
+                require("plugins.nvimtree").config()
+            end
         }
 
         use {
-          "ahmedkhalf/lsp-rooter.nvim",
-          config = function()
-            require("lsp-rooter").setup{}
-          end
+            "kyazdani42/nvim-web-devicons",
+            config = function()
+                require("plugins.icons").config()
+            end
         }
 
-    end,
-    {
-        display = {
-            border = {"┌", "─", "┐", "│", "┘", "─", "└", "│"}
+        use {
+            "nvim-telescope/telescope.nvim",
+            requires = {
+                {"nvim-lua/popup.nvim"},
+                {"nvim-lua/plenary.nvim"}
+            },
+            cmd = "Telescope",
+            config = function()
+                require("plugins.telescope").config()
+            end
         }
-    }
+
+        use {"nvim-telescope/telescope-fzf-native.nvim", run = "make", cmd = "Telescope"}
+
+        use {
+            "nvim-telescope/telescope-media-files.nvim",
+            cmd = "Telescope"
+        }
+
+        -- git stuff
+        use {
+            "lewis6991/gitsigns.nvim",
+            event = "BufRead",
+            config = function()
+                require("plugins.gitsigns").config()
+            end
+        }
+
+        -- misc plugins
+        use {
+            "windwp/nvim-autopairs",
+            after = "nvim-compe",
+            config = function()
+                require("nvim-autopairs").setup()
+                require("nvim-autopairs.completion.compe").setup(
+                    {
+                        map_cr = true,
+                        map_complete = true -- insert () func completion
+                    }
+                )
+            end
+        }
+
+        use {"andymass/vim-matchup", event = "CursorMoved"}
+
+        use {
+            "terrortylor/nvim-comment",
+            cmd = "CommentToggle",
+            config = function()
+                require("nvim_comment").setup()
+            end
+        }
+
+        use {
+            "glepnir/dashboard-nvim",
+            cmd = {
+                "Dashboard",
+                "DashboardNewFile",
+                "DashboardJumpMarks",
+                "SessionLoad",
+                "SessionSave"
+            },
+            setup = function()
+                require("plugins.dashboard").config()
+            end
+        }
+
+        use {"tweekmonster/startuptime.vim", cmd = "StartupTime"}
+
+        -- load autosave only if its globally enabled
+        use {
+            "Pocco81/AutoSave.nvim",
+            config = function()
+                require("plugins.zenmode").autoSave()
+            end,
+            cond = function()
+                return vim.g.auto_save == true
+            end
+        }
+
+        -- smooth scroll
+        use {
+            "karb94/neoscroll.nvim",
+            event = "WinScrolled",
+            config = function()
+                require("neoscroll").setup()
+            end
+        }
+
+        use {
+            "Pocco81/TrueZen.nvim",
+            cmd = {"TZAtaraxis", "TZMinimalist", "TZFocus"},
+            config = function()
+                require("plugins.zenmode").config()
+            end
+        }
+
+        --   use "alvan/vim-closetag" -- for html autoclosing tag
+
+        use {
+            "lukas-reineke/indent-blankline.nvim",
+            event = "BufRead",
+            setup = function()
+                require("utils").blankline()
+            end
+        }
+    end
 )
